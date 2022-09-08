c4ea919b73f6f444aff4903e30768741dfa16f33
Ne-eK <kartsan.nikolay@yandex.ru>
2022-09-08 11:19:11 +0300

    Seminar_3 rename

---
+++ C#_Seminar_3/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+void Func (int a) {
+    if (a == 1){
+        Console.WriteLine(" X > 0, Y > 0 ");
+    }
+    else if (a == 2){
+        Console.WriteLine(" X < 0, Y > 0 ");
+    }
+    else if (a == 3){
+        Console.WriteLine(" X < 0, Y < 0 ");
+    }
+    else if (a == 4){
+        Console.WriteLine(" X > 0, Y < 0 ");
+    }
+    else {
+        Console.WriteLine(" Введенное число не соответсвует ни одной четверти ");
+    }
+}
+Console.Write("Введите номер четверти: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Func(N);
+*/
+
+
+/*
+void Func (int x, int y) {
+    if (x > 0 && y > 0){
+        Console.WriteLine(" Первая четверть ");
+    }
+    else if (x < 0 && y > 0){
+        Console.WriteLine(" Вторая четверть ");
+    }
+    else if (x < 0 && y < 0){
+        Console.WriteLine(" Третья четверть ");
+    }
+    else if (x > 0 && y < 0){
+        Console.WriteLine(" Четвертая четверть ");
+    }
+    else {
+        Console.WriteLine(" Введенные координаты не cоответвуют условию задачи ");
+    }
+}
+Console.Write("Введите координату х больше 0: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату у больше 0: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+Func(X, Y);
+*/
+
+/*
+void Squre (int n) {
+    Console.Write(n * n + ", ");
+}
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 0){
+    Console.Write("Некорректный ввод");
+}
+else{
+    int index = 1;
+    while (index <= n) {
+        Squre(index);
+        index++;  
+    }
+}
+*/
+
